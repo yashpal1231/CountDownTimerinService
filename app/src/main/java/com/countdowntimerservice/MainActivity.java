278b3f6fca33d59129f22d0cc768008f49842a1e
yashpal1231 <yashpalvaghela1231@gmail.com>
2018-09-08 11:22:19 +0530

    put Toast

--- app/src/main/java/com/countdowntimerservice/MainActivity.java
+++ app/src/main/java/com/countdowntimerservice/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         init();
         listener();
 
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+      //  Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
     }
 
     private void init() {
